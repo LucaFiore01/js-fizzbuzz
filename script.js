ecfbfaa6849eca7d069ca8117d0cc77e30be9d34
Luca Fiore <lf44551@gmail.com>
2025-12-02 15:40:26 +0100

    chore: corretti problemi di codice

--- script.js
+++ script.js
@@ -3,10 +3,26 @@
 
 // usiamo il ciclo FOR e assegnamo ai multipli di tre il nome 'Fizz'
 
-for(let i = 1; i <= 100; i++) {
-    if (i % 3 === 0)
+
+
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 3 === 0) {
         console.log('Fizz');
+    } else {
+        console.log(i);
         
+    }
+  
+
+
+
 }
+
+
+
+
+
+
 
 
