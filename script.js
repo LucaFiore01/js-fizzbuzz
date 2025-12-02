cc60e7ab3d864aab8bbd065ba2a79f6db0ef65a3
Luca Fiore <lf44551@gmail.com>
2025-12-02 14:31:21 +0100

    primo task del pomeriggio

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// stampiamo in log i numeri da 1 a 100 
+
+// usiamo il ciclo FOR e assegnamo ai multipli di tre il nome 'Fizz'
+
+for(let i = 1; i <= 100; i++) {
+    if (i % 3 === 0)
+        console.log('Fizz');
+        
+}
+
+
