910b93b9be4d012d37ff13603dfa370978d174a1
Luca Fiore <lf44551@gmail.com>
2025-12-02 15:49:00 +0100

    feat: seconda task esercizio

--- script.js
+++ script.js
@@ -2,18 +2,19 @@
 // stampiamo in log i numeri da 1 a 100 
 
 // usiamo il ciclo FOR e assegnamo ai multipli di tre il nome 'Fizz'
-
+// all' interno del ciclo FOR, assegname alle condizioni il console.log con il nome Fizz (multipli di 3) ed il nome Buzz (multipli di 5)
+// a loro volta assegnamo alle condizioni il console.log FizzBuzz (per i multipli di 3 e di 5) 
 
 
 for (let i = 1; i <= 100; i++) {
 
     if (i % 3 === 0) {
         console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
     } else {
         console.log(i);
-        
     }
-  
 
 
 
